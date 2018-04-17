576639f054b21625074fa54df2529fb022e080d6
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 13:33:03 -0500

    tweak migrations, generate visit model

--- db/migrate/20180417181858_create_landmarks.rb
+++ db/migrate/20180417181858_create_landmarks.rb
@@ -3,10 +3,10 @@ class CreateLandmarks < ActiveRecord::Migration[5.2]
     create_table :landmarks do |t|
       t.string :name
       t.text :description
-      t.string :neighborhood
-      t.string :category
       t.string :image_link
       t.string :more_info_link
+      t.integer :neighborhood_id
+      t.integer :category_id
 
       t.timestamps
     end
