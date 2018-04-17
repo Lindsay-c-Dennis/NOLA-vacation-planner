69c3c879a8d01d22ef4cfb91e33473795650a6e2
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 16:08:38 -0500

    build basic sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id 
+			redirect_to user_path(user)
+		else 
+			flash[:notice] = "Your username or password did not match our records. Please try again."
+			render 'new'
+
+	end 
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to root_path
+	end
+
+end
