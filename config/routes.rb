b2ef44444022f51c7c37e655224ac43a52b4b4bc
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 16:26:36 -0500

    setup post sessions path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create'
   get '/signup', to: 'users#new', as: 'signup'
   resources :categories
   resources :neighborhoods
