b189c41daf580e6480f03ada33726cb37627be72
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 16:41:03 -0500

    build simple users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
 	skip_before_action :verify_login, only: [:new, :create]
 	def new
+		@user = User.new 
 	end 
 
 	def create 
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to user_path(user)
+		else 
+			flash[:notice] = "Something's wrong!"
+			render 'new'
+		end		
 	end 
 
+	def show
+		@user = User.find_by(id: session[:user_id])
+	end 
+	
+	private 
 
+	def user_params 
+		params.require(:user).permit(:username, :password, :email)
+	end		
 
 end
