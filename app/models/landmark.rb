95b1700fdbae89bbab01124643cb7cf8fd550ef1
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 13:40:43 -0500

    build landmark associations

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,2 +1,9 @@
 class Landmark < ApplicationRecord
+	has_many :visits 
+	has_many :users, through: :visits 
+	belongs_to :category
+	belongs_to :neighborhood
+
+	validates :name, presence: true, uniqueness: true 
+	
 end
