a87045d250a2854586d6c6afb97af368c8a62266
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-17 13:44:21 -0500

    build user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	has_secure_password
+	has_many :visits 
+	has_many :landmarks, through: :visits 
+
+	validates :username, presence: true, uniqueness: true 
+	validates :password, presence: true 
+	validates :email, presence: true 
 end
