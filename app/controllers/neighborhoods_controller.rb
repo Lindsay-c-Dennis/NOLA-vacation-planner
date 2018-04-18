8ef871d52b6d9ead9e1e71647a90fe7e8101feeb
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-18 13:44:28 -0500

    build out neighborhood controller

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -8,4 +8,39 @@ class NeighborhoodsController < ApplicationController
 		@neighborhood = Neighborhood.find_by(id: params[:id])
 	end	
 
+	def new
+		@neighborhood = Neighborhood.new 
+	end 
+
+	def create 
+		@neighborhood = Neighborhood.new(neighborhood_params)
+		if @neighborhood.save 
+			flash[:message] = "Neighborhood successfully created"
+			redirect_to neighborhood_path(@neighborhood)
+		else 
+			render 'new'
+		end
+	end 
+
+	def edit 
+		@neighborhood = Neighborhood.find_by(id: params[:id])
+	end 
+	
+	def update 
+		@neighborhood = Neighborhood.find_by(id: params[:id])
+		if @neighborhood.update(neighborhood_params)
+			flash[:message] = "Neighborhood successfully updated."
+			redirect_to neighborhood_path(@neighborhood)
+		else 
+			render 'edit'
+		end 
+	end 
+
+	private 
+
+	def neighborhood_params 
+		params.require(:neighborhood).permit(:name, :image_link, :description)
+	end						
+
+
 end
