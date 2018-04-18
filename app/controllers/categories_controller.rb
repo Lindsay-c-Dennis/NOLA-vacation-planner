d3285937cfa30c677b855d56ea577fb35aabd5ee
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-18 13:13:40 -0500

    category params method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,4 +35,10 @@ class CategoriesController < ApplicationController
 		@category = Category.find_by(id: params[:id])
 	end
 
+	private
+
+	def category_params 
+		params.require(:category).permit(:name)
+	end
+
 end
