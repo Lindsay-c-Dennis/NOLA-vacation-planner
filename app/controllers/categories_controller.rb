18eb009cd23aa8e356b786dcc6365de684ff703c
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-18 13:10:25 -0500

    build out categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,32 @@
 class CategoriesController < ApplicationController
 
+	def new
+		@category = Category.new 
+	end 
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save 
+			redirect_to category_path(@category)
+		else 
+			render 'new'
+		end
+	end			
+
+	
+	def edit 
+		@category = Category.find_by(id: params[:id])
+	end 
+	
+	def update 
+		@category = Category.find_by(id: params[:id])
+		if @category.update(category_params)
+			redirect_to category_path(@category)
+		else 
+			render 'edit'
+		end 
+	end					
+
 	def index
 		@categories = Category.all
 	end
