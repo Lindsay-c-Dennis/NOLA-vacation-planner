e4e9461ff42d51c3681ffe94045dae0f010c4187
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-20 14:05:13 -0500

    refactor user controller and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,29 @@
 class UsersController < ApplicationController
+	before_action :find_user, only: [:edit, :update, :show]
 	skip_before_action :verify_login, only: [:new, :create]
+	
 	def new
 		@user = User.new 
 	end 
 
 	def create 
-		user = User.new(user_params)
-		if user.save
-			session[:user_id] = user.id
-			redirect_to user_path(user)
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
 		else 
-			flash[:notice] = "Something's wrong!"
 			render 'new'
 		end		
 	end 
 
 	def show
-		@user = User.find_by(id: session[:user_id])
 		@visits = @user.visits
 	end 
 
 	def edit
-		@user = User.find_by(id: session[:user_id])
 	end	
 
 	def update
-		@user = User.find(session[:user_id])
 		@user.guide_status = params[:user][:guide_status]
 		@user.save
 		redirect_to user_path(@user)
@@ -37,4 +35,7 @@ class UsersController < ApplicationController
 		params.require(:user).permit(:name, :password, :password_confirmation, :email, :guide_status)
 	end		
 
+	def find_user 
+		@user = User.find_by(id: params[:id])
+	end
 end
