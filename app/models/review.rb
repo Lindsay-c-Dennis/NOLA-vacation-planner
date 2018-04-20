07d49a360123aac4b5cd26dfc6e3c2ad2a295d86
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-20 13:27:07 -0500

    add timestamp to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ApplicationRecord
 	validates :content, presence: true, uniqueness: true 
 
 	scope :most_recent, -> (limit) { order("created_at desc").limit(limit) }
+
+	def posted_at
+  		created_at.strftime("Review posted %A, %b %e, at %l:%M %p")
+	end
 end
