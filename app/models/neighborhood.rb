c1278bd5e1b31b4dd8ec8f5eb7bf188e98eb662a
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-11 13:06:49 -0500

    add validation for image for neighorhood

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,6 @@
 class Neighborhood < ApplicationRecord
 	has_many :landmarks
 
-	validates :name, presence: true, uniqueness:true 
+	validates :name, presence: true, uniqueness:true
+	validates :image_link, presence: true 
 end
