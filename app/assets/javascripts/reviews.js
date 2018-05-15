263a6b1da42e9eabb917326d9960de7f01713ec1
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-15 13:03:52 -0500

    begin implementing review class

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -9,8 +9,8 @@ function attachListeners() {
 		$('#landmark-reviews').empty();
 		const url = this.href
 		$.get(`${url}.json`, function(reviews) {
-			debugger
-			reviews.forEach(renderReview);
+			//debugger
+			reviews.map(review => renderReview(review));
 		});
 	});
 
@@ -26,14 +26,11 @@ function attachListeners() {
 		e.preventDefault();
 		const editUrl = this.href
 		$.get(`${editUrl}`, function(data) {
-			//debugger
 			$('#review-content').html(data)
 		});
 	});
 
-	
 	$(document).on('submit', '#new_review', function(e){
-		debugger
 		e.preventDefault(); 
 		let url = $(this).attr('action')
 		let landmarkId = $('#review_landmark_id').val();
@@ -41,26 +38,49 @@ function attachListeners() {
 		let reviewText = $(this).find('#review_content').val();
 		let data = $(this).serializeArray();
 		$.post(url, data).done(function(review) {
-			debugger
-			$('#landmark-reviews').prepend(renderReview(review));
+			newRev = new Review(review.user.id, review.user.name, review.landmark.id, review.landmark.name, review.content, review.created_at);
+			$('#landmark-reviews').prepend(newRev.renderReview());
 			$('#new-review-form').empty();
 		});
 	});
-				
 
-
+	$(document).on('submit', '#edit_review', function(e) {
+		e.preventDefault();
+		let patchUrl = $(this).attr('action');
+		let editData = $(this).serializeArray();
+		debugger
+		$.ajax({
+			type: 'PATCH',
+			url: patchURL,
+			data: editData
+		});
+	})
 }
+
+class Review {
+		constructor(userId, userName, landmarkId, landmarkName, content, createdAt) {
+			this.userId = userId;
+			this.userName = userName
+			this.landmarkId = landmarkId;
+			this.landmarkName = landmarkName
+			this.content = content
+			this.createdAt = createdAt
+		}
+	}
+
+	Review.prototype.renderReview = function() {
+			debugger
+			let postTime = moment(this.createdAt).format('LLL')
+			return `
+				<h3> On <a href='/landmarks/${this.landmarkId}'>${this.landmarkName}</a>, ${this.userName} says: </h3>
+				<p>${this.content}</p>
+				<h6><em>Review posted ${postTime}</em></h6>
+				
+		<br /> `
+		}	
 
 function printReview(review) {
 	$('#landmark-reviews').append(`<li><strong>${review.user.name} says:</strong> ${review.content}</li>`);
 }
 
-function renderReview(review) {
-			let postTime = moment(review.created_at).format('LLL')
-			return `
-				<h3> On <a href='/landmarks/${review.landmark.id}'>${review.landmark.name}</a>, ${review.user.name} says: </h3>
-				<p>${review.content}</p>
-				<h6><em>Review posted ${postTime}</em></h6>
-				
-		<br /> `
-		}
+
