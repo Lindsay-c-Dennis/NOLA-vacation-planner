92132b3caf83f6697a48d8637bc65df643e95888
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-15 11:42:45 -0500

    add current user method

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,9 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :content, :created_at
+  attributes :id, :content, :created_at, :cu
   belongs_to :landmark
   belongs_to :user
+
+  def cu 
+  	return current_user
+  end
 end
